4703613ea69eb765e4bfc0c9a6cff16295915ed8
Lina Shl <Lina Shl>
2016-07-16 16:53:31 +0800

    add the exercises from one to five

---
+++ ex1.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8-*-
+"""
+    @Author: ShiLiNa
+    @Brief: The exercise of Learn python the hard way
+    @CreatedTime: 13/7/2016
+"""
+print "Hello World"
+print "Hello Again"
+print "I like typing like this"
+print "This is fun"
+print "Yay! Printing"
+print "I'd much rather you 'not.'"
+print 'I "said" do not touch this'
+print "I am cute huahua"
